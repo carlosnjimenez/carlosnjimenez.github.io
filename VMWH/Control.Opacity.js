2be0f9017593ecfca20c590d1f2a9812c1551b6f
carlosnjimenez <carlos.nelson.jimenez@googlemail.com>
2016-11-23 21:52:35 -0300

    VMWH: agregar controles

--- VMWH/Control.Opacity.js
+++ VMWH/Control.Opacity.js
@@ -9,45 +9,6 @@
 //Declare global variables
 var opacity_layer;
 
-//Create a control to increase the opacity value. This makes the image more opaque.
-L.Control.higherOpacity = L.Control.extend({
-    options: {
-        position: 'bottomleft'
-    },
-    setOpacityLayer: function (layer) {
-            opacity_layer = layer;
-    },
-    onAdd: function () {
-        
-        var higher_opacity_div = L.DomUtil.create('div', 'higher_opacity_control');
-
-        L.DomEvent.addListener(higher_opacity_div, 'click', L.DomEvent.stopPropagation)
-            .addListener(higher_opacity_div, 'click', L.DomEvent.preventDefault)
-            .addListener(higher_opacity_div, 'click', function () { onClickHigherOpacity() });
-        
-        return higher_opacity_div;
-    }
-});
-
-//Create a control to decrease the opacity value. This makes the image more transparent.
-L.Control.lowerOpacity = L.Control.extend({
-    options: {
-        position: 'topright'
-    },
-    setOpacityLayer: function (layer) {
-            opacity_layer = layer;
-    },
-    onAdd: function (map) {
-        
-        var lower_opacity_div = L.DomUtil.create('div', 'lower_opacity_control');
-
-        L.DomEvent.addListener(lower_opacity_div, 'click', L.DomEvent.stopPropagation)
-            .addListener(lower_opacity_div, 'click', L.DomEvent.preventDefault)
-            .addListener(lower_opacity_div, 'click', function () { onClickLowerOpacity() });
-        
-        return lower_opacity_div;
-    }
-});
 
 //Create a jquery-ui slider with values from 0 to 100. Match the opacity value to the slider value divided by 100.
 L.Control.opacitySlider = L.Control.extend({
@@ -67,7 +28,7 @@ L.Control.opacitySlider = L.Control.extend({
               range: "min",
               min: 0,
               max: 100,
-              value: 60,
+              value: 100,
               step: 10,
               start: function ( event, ui) {
                 //When moving the slider, disable panning.
@@ -90,37 +51,4 @@ L.Control.opacitySlider = L.Control.extend({
         return opacity_slider_div;
     }
 });
-
-
-function onClickHigherOpacity() {
-    var opacity_value = opacity_layer.options.opacity;
-    
-    if (opacity_value > 1) {
-        return;
-    } else {
-        opacity_layer.setOpacity(opacity_value + 0.2);
-        //When you double-click on the control, do not zoom.
-        map.doubleClickZoom.disable();
-        map.once('click', function (e) { 
-            map.doubleClickZoom.enable();
-        });
-    }
-
-}
-
-function onClickLowerOpacity() {
-    var opacity_value = opacity_layer.options.opacity;
-    
-    if (opacity_value < 0) {
-        return;
-    } else {
-        opacity_layer.setOpacity(opacity_value - 0.2);
-        //When you double-click on the control, do not zoom.
-        map.doubleClickZoom.disable();
-        map.once('click', function (e) { 
-            map.doubleClickZoom.enable();
-        });
-    }
-      
-}
 
