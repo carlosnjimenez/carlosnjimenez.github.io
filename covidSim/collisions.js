19f18866a8d25fbf028b43332998fa8315d79198
carlosnjimenez <carlos.nelson.jimenez@googlemail.com>
2020-04-06 09:25:01 -0300

    covid

---
+++ covidSim/collisions.js
@@ -0,0 +1,13 @@
+const { hypot } = Math
+
+export const calculateChangeDirection = ({ dx, dy }) => {
+  const hyp = hypot(dx, dy);
+  const ax = dx / hyp;
+  const ay = dy / hyp
+  return { ax, ay }
+}
+
+export const checkCollision = ({ dx, dy, diameter }) => {
+  const distance2 = dx * dx + dy * dy
+  return distance2 < diameter * diameter
+}
---
+++ covidSim/options.js
@@ -0,0 +1,59 @@
+const DEFAULT_FILTERS = {
+  death: false,
+  stayHome: false
+}
+
+export const CANVAS_SIZE = {
+  height: 880,
+  width: 360
+}
+
+export const DESKTOP_CANVAS_SIZE = {
+  height: 400,
+  width: 800
+}
+
+export const BALL_RADIUS = 5
+export const COLORS = {
+  death: '#F44336',
+  recovered: '#9C27B0',
+  infected: '#009688',
+  well: '#03A9F4'
+}
+
+export const STATES = {
+  infected: 'infected',
+  well: 'well',
+  recovered: 'recovered',
+  death: 'death'
+}
+
+export const COUNTERS = {
+  ...STATES,
+  'max-concurrent-infected': 'max-concurrent-infected'
+}
+
+export const STARTING_BALLS = {
+  [STATES.infected]: 1,
+  [STATES.well]: 199,
+  [STATES.recovered]: 0,
+  [STATES.death]: 0,
+  'max-concurrent-infected': 0
+}
+
+export const RUN = {
+  filters: { ...DEFAULT_FILTERS },
+  results: { ...STARTING_BALLS },
+  tick: 0
+}
+
+export const MORTALITY_PERCENTATGE = 5
+export const SPEED = 1
+export const TOTAL_TICKS = 1600
+export const TICKS_TO_RECOVER = 500
+export const STATIC_PEOPLE_PERCENTATGE = 25
+
+export const resetRun = () => {
+  RUN.results = { ...STARTING_BALLS }
+  RUN.tick = 0
+}
